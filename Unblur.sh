a9716d51578ca214ad97879636c8ad2250056db8
noorhaq <47422949+noorhaq@users.noreply.github.com>
2019-04-16 23:40:37 +0500

    Update Unblur.sh

--- Unblur.sh
+++ Unblur.sh
@@ -1,8 +1,8 @@
 cd TOOLS
 
-wget --no-check-certificate 'http://grigoriefflab.janelia.org/sites/all/modules/pubdnldcnt/pubdnldcnt.php?file=http://grigoriefflab.janelia.org/sites/default/files/unblur_1.0.2.tar.gz&nid=4902' -O unblur.zip
-unzip unblur.zip
+wget --no-check-certificate 'http://grigoriefflab.janelia.org/sites/all/modules/pubdnldcnt/pubdnldcnt.php?file=http://grigoriefflab.janelia.org/sites/default/files/unblur_1.0.2.tar.gz&nid=4902' -O unblur.tar.gz
+tar -xf unblur.tar.gz
 
 
-rm -r unblur.zip
+rm -r unblur.tar.gz
 cd ..
