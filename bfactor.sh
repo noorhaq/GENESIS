9e132633c762465bdd4c1854b8cbec031c7313a8
noorhaq <47422949+noorhaq@users.noreply.github.com>
2019-05-09 01:20:16 +0500

    Delete bfactor.sh

--- bfactor.sh
+++
@@ -1,9 +0,0 @@
-
-#		Installation script file for bfactor
-cd TOOLS
-wget --no-check-certificate 'http://grigoriefflab.janelia.org/sites/all/modules/pubdnldcnt/pubdnldcnt.php?file=http://grigoriefflab.janelia.org/sites/default/files/bfactor_v1.03.tar.gz&nid=129' -O bfactor_v1.03.tar.gz
-
-tar -xf bfactor_v1.03.tar.gz
-
-rm -r bfactor_v1.03.tar.gz
-cd .. 
