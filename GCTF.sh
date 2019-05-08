855aa531261090d077452f2660bfae650809191e
noorhaq <47422949+noorhaq@users.noreply.github.com>
2019-05-09 01:18:26 +0500

    Delete GCTF.sh

--- GCTF.sh
+++
@@ -1,9 +0,0 @@
-#	Installation Package for frealign
-cd TOOLS
-
-wget --no-check-certificate 'http://www.mrc-lmb.cam.ac.uk/kzhang/Gctf/Gctf_v1.06_and_examples.tar.gz' -O Gctf.tar.gz
-
-tar -xf Gctf.tar.gz
-
-rm -r Gctf.tar.gz
-cd ..
