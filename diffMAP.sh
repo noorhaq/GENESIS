43c9b659d85139b3815767a281883faa92762595
noorhaq <47422949+noorhaq@users.noreply.github.com>
2019-05-09 01:19:54 +0500

    Delete diffMAP.sh

--- diffMAP.sh
+++
@@ -1,9 +0,0 @@
-#		Installation script file for diffMAP
-cd TOOLS
-
-wget --no-check-certificate 'http://grigoriefflab.janelia.org/sites/all/modules/pubdnldcnt/pubdnldcnt.php?file=http://grigoriefflab.janelia.org/sites/default/files/diffmap.tar.gz&nid=132' -O diffmap.tar.gz
-
-tar -xf diffmap.tar.gz
-
-rm -r diffmap.tar.gz
-cd ..
