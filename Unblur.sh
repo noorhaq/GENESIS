fc6214feabd26c7db81d193dc2380afc66ec69b5
noorhaq <47422949+noorhaq@users.noreply.github.com>
2019-05-09 01:20:27 +0500

    Delete Unblur.sh

--- Unblur.sh
+++
@@ -1,8 +0,0 @@
-cd TOOLS
-
-wget --no-check-certificate 'http://grigoriefflab.janelia.org/sites/all/modules/pubdnldcnt/pubdnldcnt.php?file=http://grigoriefflab.janelia.org/sites/default/files/unblur_1.0.2.tar.gz&nid=4902' -O unblur.tar.gz
-tar -xf unblur.tar.gz
-
-
-rm -r unblur.tar.gz
-cd ..
